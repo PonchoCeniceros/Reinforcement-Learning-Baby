567f56b77611cc4ea9d81700397bc3e810d1e2c8
PonchoCeniceros <gio.icc.uach@gmail.com>
2021-02-28 20:19:30 -0700

    DQN and SARSA agents have been developed

--- apps/cartPole/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-# no mostrar alertas de tensorflow
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
-from .agents import DQNAgent
-# solo para python 3.8 o >
-# from .helloWorld import helloWorld, simpleAction
